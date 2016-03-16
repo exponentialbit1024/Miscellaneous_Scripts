4aa47678678d07c9f01684784f654b0d99e78460
Sid Shah <shah255@purdue.edu>
2016-03-15 23:40:05 -0500

    tesseract with future opencv inclusion

--- letterRecog.py
+++ letterRecog.py
@@ -1,8 +1,13 @@
 #for Mac use brew install pillow for install PIL and then install pytesseract as sudo pip install pytesseract
-
+import numpy as np
+import cv2
 try:
 	import Image
 except:
 	from PIL import Image
 import pytesseract
-print(pytesseract.image_to_string(Image.open('diffTest3.jpg')))
+pilImage = Image.open('diffTest3.jpg')
+
+print(pytesseract.image_to_string(pilImage))
+
+imgArray = np.array(pilImage.getdata(),np.uint8).reshape(pilImage.size[1], pilImage.size[0], 3)
